ee18650b91be7850d4570919ce106a67a7675241
changhuilong <1032326058@qq.com>
2022-08-11 22:58:21 +0800

    支付微服务集群配置

--- cloud-consumer-order80/src/main/java/com/atguigu/springboot/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springboot/controller/OrderController.java
@@ -14,7 +14,8 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 //    单机版
-    public static final String PAYMENT_URL = "http://127.0.0.1:8001";
+//    public static final String PAYMENT_URL = "http://127.0.0.1:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
