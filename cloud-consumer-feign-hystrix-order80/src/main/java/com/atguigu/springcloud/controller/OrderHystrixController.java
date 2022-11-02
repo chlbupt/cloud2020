720e30bc644a9f15f45e9eee2761579679d5bdc8
changhuilong <1032326058@qq.com>
2022-11-02 14:16:26 +0800

    Hystrix全局服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("consumer")
 @Slf4j
+@DefaultProperties(defaultFallback = "paymentGlobalFallbackMethod")// hystrix 全局fallback方法
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -25,15 +27,21 @@ public class OrderHystrixController {
     }
 
     @GetMapping("/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod="paymentTimeoutFallbackMethod", commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="1500")
-    })
+//    @HystrixCommand(fallbackMethod="paymentTimeoutFallbackMethod", commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="1500")
+//    })
+    @HystrixCommand
     public String paymentInfoTimeout(@PathVariable("id") Integer id){
+        int age = 10 / 0;
         String result = paymentHystrixService.paymentInfoTimeout(id);
         return result;
     }
 
     public String paymentTimeoutFallbackMethod(@PathVariable("id") Integer id){
         return "我是消费者80，对方支付系统繁忙请10秒后再试或者自己运行出错检查自己，/(ㄒoㄒ)/~~";
+    }
+
+    public String paymentGlobalFallbackMethod() {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 }
