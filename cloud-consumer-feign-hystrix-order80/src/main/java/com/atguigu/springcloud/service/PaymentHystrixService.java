1626da845fe5461fb2d7c1a341b4138fbd1260f6
changhuilong <1032326058@qq.com>
2022-11-02 14:38:35 +0800

    Hystrix服务降级类的使用

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentHystrixService.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.service;
 
+import com.atguigu.springcloud.service.impl.PaymentFallbackServiceImpl;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(value="CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+//@FeignClient(value="CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value="CLOUD-PROVIDER-HYSTRIX-PAYMENT", fallback = PaymentFallbackServiceImpl.class)
 public interface PaymentHystrixService {
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfoOk(@PathVariable("id") Integer id);
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/impl/PaymentFallbackServiceImpl.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.PaymentHystrixService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackServiceImpl implements PaymentHystrixService {
+    @Override
+    public String paymentInfoOk(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfoTimeout(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
